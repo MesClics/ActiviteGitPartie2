336f64bf56a2b3c691bfa4edceb03fdceb2b7a18
MesClics <mesclics@hotmail.com>
2016-01-05 13:53:57 +0100

    Codage du footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+    echo'
+    <p>Page réalisée dans le cadre du cours <a href="https://openclassrooms.com/courses/gerer-son-code-avec-git-et-github">Gérer son code avec Git et Github</a> sur <a href="http://www.openclassrooms.com">Openclassrooms.com</a></p>
+    
+    <p>Réalisation : <a href="http://www.mesclics.fr">MesClics</a> - ' . date('Y') . '</p>
+    <address><a href="mailto:contact@mesclics.fr">contact@mesclics.fr</a></address>';
+
+?>
+    
